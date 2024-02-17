e30cdfea92b7488fafed6e7d31227e7c025e0656
Khan Kamran Aslam <153853096+KhanKamranAslam@users.noreply.github.com>
2024-02-17 15:37:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["FrontEnd Developer", "Java Developer", "Web Developer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
